7b904b39ad931e32c98c9a52387d7a73b6e36d21
Phuc <nguyenduythienphuc71@gmail.com>
2025-02-11 22:36:12 +0700

    Commit các file obj đã sửa đổi

--- obj/Debug/net8.0/Project-Sem3.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project-Sem3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-Sem3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99ebc3d36d456a07cf488e02f7d1b8ebac20557d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b727c1c7ad2af08436915f870250775d962410ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-Sem3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-Sem3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
